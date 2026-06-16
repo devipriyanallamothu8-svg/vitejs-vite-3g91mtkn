dabaa4077726d4542c77661eb7ec7a07c712c62d
devipriyanallamothu8-svg <devipriyanallamothu8@gmail.com>
2026-06-16 11:09:37 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Hi, I'm Devipriya</h1>
+      <h2>Full Stack Developer</h2>
+
+      <section>
+        <h3>About Me</h3>
+        <p>
+          I am a Full Stack Web Developer skilled in React, Node.js, Express.js,
+          and MongoDB.
+        </p>
+      </section>
+
+      <section>
+        <h3>Skills</h3>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>React.js</li>
+          <li>Node.js</li>
+          <li>MongoDB</li>
+        </ul>
+      </section>
+
+      <section>
+        <h3>Projects</h3>
+        <div className="card">
+          <h4>Internship Portal</h4>
+          <p>MERN Stack Project</p>
+        </div>
+      </section>
+
+      <section>
+        <h3>Contact</h3>
+        <p>Email:devipriyanallamothu8@gmail.com</p>
+      </section>
+    </div>
+  );
+}
+
+export default App;
